247b462459d46e9c3f6aed7b2180ad2c5a66fdb7
gigaastral <carboneandrea317@gmail.com>
2025-12-04 17:17:59 -0500

    Initial kernel source upload

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#3 SMP PREEMPT Thu Dec 4 12:14:55 EST 2025"
+#define LINUX_COMPILE_BY "@ravindu644"
+#define LINUX_COMPILE_HOST "kali"
+#define LINUX_COMPILER "Android (7284624, based on r416183b) clang version 12.0.5 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee), LLD 12.0.5 (/buildbot/src/android/llvm-toolchain/out/llvm-project/lld c935d99d7cf2016289302412d708641d52d2f7ee)"
